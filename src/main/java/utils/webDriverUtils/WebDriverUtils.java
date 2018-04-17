df208fe4ae8c16c5db266ee590b51eb14f7d0a40
George Helidze <>
2018-04-17 12:50:12 +0300

    Test another chrome config

--- src/main/java/utils/webDriverUtils/WebDriverUtils.java
+++ src/main/java/utils/webDriverUtils/WebDriverUtils.java
@@ -80,7 +80,7 @@ public class WebDriverUtils {
                                 chromeDriver = "chromedriver";
                             }
 
-                            pathToChromeDriver = WebDriverUtils.class.getClassLoader().getResource("chrome_profile/" + chromeDriver).getPath();
+                            pathToChromeDriver = "/Users/georgehelidze/Fabulive_Automation/src/main/resources/chrome_profile/chromedriver";//WebDriverUtils.class.getClassLoader().getResource("chrome_profile/" + chromeDriver).getPath();
 
 
                             System.out.println("-> Try create ChromeDriver!... ");
